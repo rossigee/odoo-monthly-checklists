0c2d0e6d827104a60e9e8d708ea2bb8b24d30e0a
Ross Golder <ross@golder.org>
2026-09-01 12:14:15 +0700

    fix: update monthly_compliance_checklist version to 17.0 for Odoo 17 compatibility

--- monthly_compliance_checklist/__manifest__.py
+++ monthly_compliance_checklist/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Monthly Compliance Checklist",
-    "version": "16.0.1.1.0",
+    "version": "17.0.1.1.0",
     "author": "Ross Golder",
     "website": "https://golder.org/",
     "license": "AGPL-3",
